0bb5eed16273b7fb7ab51336a5af801ec983daa7
Paul <mrpaulb@gmail.com>
2014-03-19 11:50:11 +0000

    test added to home static_page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,7 +3,8 @@ require 'spec_helper'
 describe "Static pages" do
   describe "home page" do
     it "should have the content 'Sample App'" do
-      # visit 
+      visit '/static_pages/home'
+      page.should have_content('Sample App')
     end
     #it "works! (now write some real specs)" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
